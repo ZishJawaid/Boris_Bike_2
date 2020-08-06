3b84a7c2d541d3e2981d4acc7d2bdc2bf8b5913c
Zish Jawaid <zisheanj@gmail.com>
2020-08-06 17:02:21 +0100

    Ch13 raise error unit test complete

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -9,17 +9,35 @@ describe DockingStation do
     expect(DockingStation.new).to respond_to(:release_bike)
   end
 
-  it 'raises an error if no bikes in docking station' do
-    station = DockingStation.new
-    expect { station.release_bike }.to raise_error ("No bikes in docking station")
+  describe '#release_bike' do
+
+    it 'raises an error if no bikes in docking station' do
+      station = DockingStation.new
+      expect { station.release_bike }.to raise_error ("No bikes in docking station")
+    end
+
+    it 'releases a bike' do
+      station = DockingStation.new
+      bike = Bike.new
+      station.dock(bike)
+      expect(station.release_bike).to eq bike
+    end
+
   end
 
-  it 'Expects an already docked bike to be released' do
-    station = DockingStation.new
-    bike = Bike.new
-    station.dock(bike)
-    expect(station.release_bike).to eq bike
+
+  it 'raises an error if more than one bike is docked' do
+
+      station = DockingStation.new
+      bike_1 = Bike.new
+      station.dock(bike_1)
+      bike_2 = Bike.new
+      expect { station.dock(bike_2) }.to raise_error ("Only one bike can be docked")
+
   end
+
+
+
 
   it 'responds to dock method with 1 argument' do
     station = DockingStation.new
